e1f1b44f9ed6adc178c8f9ce7e46f6f372103b4c
Nacho Daibes <juan.daibes@segurossura.com.ar>
2023-09-04 18:18:20 -0300

    dto register agrandado

--- src/auth/dto/loginAuth.dto.ts
+++ src/auth/dto/loginAuth.dto.ts
@@ -1,12 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsEmail, MaxLength, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class LoginAuthDto{
 
     @IsEmail()
+    @IsNotEmpty()
     email: string
 
     @MinLength(4)
     @MaxLength(12)
+    @IsNotEmpty()
+    @IsString()
     password: string
 }
--- src/auth/dto/registerAuth.dto.ts
+++ src/auth/dto/registerAuth.dto.ts
@@ -1,11 +1,15 @@
 import { PartialType } from "@nestjs/swagger";
 import { LoginAuthDto } from "./loginAuth.dto";
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 
 export class RegisterAuthDto extends PartialType(LoginAuthDto) {
 
     @IsNotEmpty()
+    @IsString()
     username: string
 
+    @IsString()
+    @IsOptional()
+    image: string
 }
