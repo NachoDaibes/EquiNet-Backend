9f7cef723372c74413a11537debc4cdca274455a
Andres <andreslxvi@gmail.com>
2023-11-06 11:15:46 -0300

    Avances

--- src/entities/location.entity.ts
+++ src/entities/location.entity.ts
@@ -1,20 +1,30 @@
-import { Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm"
-import { Department } from "./department.entity"
-import { Publication } from "./publication.entity"
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Department } from './department.entity';
+import { Publication } from './publication.entity';
 
-@Entity({name: 'Location'})
-export class Location{
+@Entity({ name: 'Location' })
+export class Location {
+  @PrimaryGeneratedColumn('increment', { name: 'id' })
+  id: number;
 
-    @PrimaryGeneratedColumn('increment', {name: 'id'})
-    id: number
+  @Column({ name: 'name' })
+  name: string;
 
-    @Column({name: 'name'})
-    name: string
+  @Column({ name: 'status', nullable: true })
+  status: boolean;
 
-    @ManyToOne(() => Department, (department) => department.location)
-    @JoinColumn({name: 'department_Id'})
-    department: Department
+  @ManyToOne(() => Department, (department) => department.location)
+  @JoinColumn({ name: 'department_Id' })
+  department: Department;
 
-    @OneToMany(() => Publication, (publication) => publication.location)
-    publication: Publication[]
+  @OneToMany(() => Publication, (publication) => publication.location)
+  publication: Publication[];
 }
